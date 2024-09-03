c9ab2cbe73bfd9c03861acd0af1c9adfc33e2aa3
vijayraj627 <vijayraj627@gmail.com>
2024-09-03 20:01:42 +0530

    code pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+
+  const [inputData, setInputData] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleClick = () => {
+    setData([...data, inputData]);
+  } 
+
+  const remove = (i) => {
+    const rem = data.filter((_,ind)=>(
+        ind !== i
+    ))
+    setData(rem)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My ToDo App</h1>
+      <div className="main">
+        <input type="text" placeholder="Add Your ToDo List" value={inputData} onChange={(e)=>setInputData(e.target.value)}/>
+        <button onClick={handleClick}>+</button>
+      </div>
+      <div className="allList">
+        {data.map((item,i)=>{
+          return <p key={i}>{item}<span onClick={()=>remove(i)}>Remove</span></p>
+        })}
+          
+      </div>
+      
     </div>
   );
 }
